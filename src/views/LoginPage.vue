d0476eba376bfe002fe553d0f8ff14be39fe4f38
japhethnyaranga <nyaranga4@gmail.com>
2023-04-27 20:19:59 +0300

    Added bootstrap and created routing to the application

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" v-model="email" required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" v-model="password" required />
+        </div>
+        <div>
+          <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+      };
+    },
+    methods: {
+      loginpage() {
+        // Implement login functionality here
+        console.log('login pressed')
+      },
+    },
+  };
+  </script>
+  
