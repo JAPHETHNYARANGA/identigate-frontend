eff38b5f9ed4927ac4e68490e71fc15567a4ade8
japhethnyaranga <nyaranga4@gmail.com>
2023-04-27 23:20:08 +0300

    created side nav and two images

---
+++ src/views/DashBoard.vue
@@ -0,0 +1,51 @@
+<template>
+    <Sidebar />
+
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    this side
+                    <btton class="btn btn-primary">hello</btton>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <img src="../assets/growth.png" alt="My Image">
+                    <h5>Users</h5>
+
+                    <img src="../assets/growth.png" alt="My Image">
+                    <h5>Total Products</h5>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+import Sidebar from '../views/Sidenav.vue'
+
+export default {
+    name: 'ParentView',
+    components: {
+        Sidebar
+    }
+}
+
+</script>
+
+<style scoped>
+.main {
+    margin-left: 160px;
+    /* Same as the width of the sidenav */
+    font-size: 28px;
+    /* Increased text to enable scrolling */
+    padding: 0px 10px;
+}
+
+img{
+    margin-top: 3%;
+    height: 20%;
+}
+</style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -27,7 +27,7 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button type="submit" class="btn btn-primary">Register</button>
                 </div>
 
 
@@ -66,6 +66,7 @@ h6 {
     font-weight: 600;
     font-size: 17px;
     cursor: pointer;
+    text-decoration: none;
     
 }
 
