66dd120f411f4b81fccf7226954b46b764ce3fa8
japhethnyaranga <nyaranga4@gmail.com>
2023-04-28 12:25:47 +0300

    create item functionality successfully created

--- src/views/CreateItem.vue
+++ src/views/CreateItem.vue
@@ -4,7 +4,33 @@
     <div class="main">
         <div class="container-fluid">
             <div class="row">
-              <h5>Create Product</h5>
+                <h5>Create Product</h5>
+
+                <form class="center" @submit.prevent="create">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            v-model="name" required>
+
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Password</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            v-model="description" required>
+
+                    </div>
+
+                    <label class="form-label" for="customFile">Add Image</label>
+                    <input type="file" class="form-control" id="customFile" />
+
+                    <div>
+                        <button type="submit" class="btn btn-primary">Add Product</button>
+                    </div>
+
+
+
+                </form>
             </div>
 
         </div>
@@ -23,16 +49,50 @@ export default {
         Sidebar
     },
 
-  
+    data() {
+        return {
+            name: '',
+            description: ''
+        }
 
-  beforeRouteEnter(to, from, next) {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      next('/'); // Redirect to login page
-    } else {
-      next();
+
+    },
+    methods: {
+        async create() {
+            const name = this.name;
+            const description = this.description;
+            const response = await fetch("http://127.0.0.1:8000/api/items", {
+                method: "POST",
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name , description })
+            });
+
+            const data = await response.json();
+
+            // check if post was successful
+            if (data.success) {
+               
+               
+                console.log("Success:", data.message);
+                this.$router.push({ path: '/products' });
+            } else {
+                console.error(" failed:", data.message);
+            }
+        }
+    },
+
+
+    beforeRouteEnter(to, from, next) {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            next('/'); // Redirect to login page
+        } else {
+            next();
+        }
     }
-  }
 }
 
 </script>
@@ -49,5 +109,13 @@ export default {
 img {
     margin-top: 3%;
     height: 20%;
+}
+
+form {
+    width: 50%;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: 5%;
 }
 </style>
